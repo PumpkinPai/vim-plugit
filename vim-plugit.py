6e445af826dfda2f1e66455626196299b624d554
pumpkinpai <pumpkinson@gmail.com>
2015-05-14 10:10:21 -0400

    realizing I can't get away from python

--- vim-plugit.py
+++ vim-plugit.py
@@ -1,14 +1,15 @@
-#!/bin/bash
+#!/usr/bin/python3
 
-# Run this in a directory to create an empty vim plugin with filenames that match user input on first run.
-# This file will delete itself after running.
+###############################################################################
+# Run this in a directory to create an empty vim plugin with
+# filenames that match user input on first run.
+# This file and its cohorts will delete itself after running.
+###############################################################################
+
+import os
 
 # VARIABLES
-directories="ftplugin ftdetect indent syntax"
-files="ftplugin
-
-for dir in $directories; do
-    mkdir dir
-done
+directories = ['ftplugin', 'ftdetect', 'indent', 'syntax']
+files = ['ftplugin.vim', 'ftdetect.vim', 'indent.vim', 'syntax.vim']
 
 
