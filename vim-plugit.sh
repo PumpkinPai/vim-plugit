49f84729540c2d7977f85dafd85cfc5974cfdd5a
pumpkinpai <pumpkinson@gmail.com>
2015-05-13 21:55:43 -0400

    First real commit.  I mean it this time, baby.

---
+++ vim-plugit.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Run this in a directory to create an empty vim plugin with filenames that match user input on first run.
+# This file will delete itself after running.
+
+# VARIABLES
+directories="ftplugin ftdetect"
+mkdir 
